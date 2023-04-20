2b479c1b330d66a6886288a210a3d434277624bf
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-04-20 14:26:47 +0300

    Complete

--- Program.cs
+++ Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
+            /*
+            int numOfStrings = 1000;
+            int numOfBlockedStrings = 10;
+
+            string[] testStrings;
+            string[] blockedTestStrings=new string[numOfBlockedStrings];
+            HostGenerator generator = new HostGenerator();
             
-        }
+            testStrings = generator.GenerateStringsArray(numOfStrings);
+            testStrings=testStrings.ToList().Distinct().ToArray();
+            foreach (var str in testStrings)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine();
+            
+            for (int i = 0; i < numOfBlockedStrings; i++)
+            {
+                blockedTestStrings[i]=testStrings[i];
+            }
+            foreach (var str in blockedTestStrings)
+            {
+                Console.WriteLine(str);
+            }
+
+            int[] allowedHosts = Solution.solution(testStrings, blockedTestStrings);
+            */
+
+            string[] a = 
+            { 
+                "unlock.microvirus.md",
+                "visitwar.com", 
+                "visitwar.de", 
+                "fruonline.co.uk", 
+                "australia.open.com", 
+                "credit.card.us",
+            };                                                                                                                  
+            string[] b = 
+            { 
+                "com",
+                "microvirus.md",
+                "visitwar.de",
+                "piratebay.co.uk",
+                "list.stolen.credit.card.us",
+            };
+
+            int[] allowedHosts = Solution.solution(a, b);
+            Console.WriteLine("Indexes of allowed domains:");
+            for (int i = 0; i < allowedHosts.Length; i++)
+                Console.Write(allowedHosts[i] + " ");
+
+            Console.WriteLine();
+            Console.WriteLine(Solution.counter);
+        }                                                                                                                                                                                           
     }
-}
+}                                                          
---
+++ Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace SimpleCodeTestTask;
+
+public class Solution
+{
+    public static int counter = 0;
+
+    public static int[] solution(string[] A, string[] B)
+    {
+        bool IsBlocked(string[] hostString, HashSet<string> hashSet)
+        {
+            for (int j = hostString.Length - 1; j >= 0; j--)
+            {
+                //counter++;
+                var connect = string.Join(".", hostString.Skip(j));
+                if (hashSet.Contains(connect)) return true;
+            }
+            return false;
+        }
+
+        var forbiddenHosts = new HashSet<string>(B);
+        var allowedIndices = new List<int>();
+
+        for (var i = 0; i < A.Length; i++)
+        {
+            var hostString = A[i].Split('.');
+            if (!IsBlocked(hostString, forbiddenHosts))
+            {
+                allowedIndices.Add(i);
+            }
+        }
+        return allowedIndices.ToArray();
+    }
+}
